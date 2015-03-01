ec8363fc8e16941c03c2d0f0fc6d2677763ab563
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-02-28 22:56:41 -0800

    Problem3 failed so bad :( There is such an easy way to do it - problem3_1.py

--- HW3/problem3.py
+++ HW3/problem3.py
@@ -2,89 +2,87 @@ import numpy as np
 import pylab as plt
 from sympy.solvers import solve
 from sympy import Symbol
+from scipy.optimize import fsolve
 
-###############
-# a
-###############
+#########################################
+# answer to part a of the question
+###################################
+plt.hold('on')
 
-mu_a = np.mat([[1], [1]])
-sigma_a = np.mat([[2, 0], [0,1]])
-
-# p = 0.1
-
-# constant = -2*np.log(2*np.pi*np.sqrt(np.linalg.det(sigma_a))*p)
-
-# print mu_a.shape, sigma_a.shape, constant
-
-
-def isocontourer(x2, constant):
-    x = Symbol('x')
-    return(solve(2*x**2+8*x+3*x2**2-14*x2-2*x*x2-constant))
-# 
-# for constant in [0, 1, 10]:
-#     plt.plot(*class2(np.linspace(0, 1, 100), np.linspace(0, 1, 100), constant))
-# print "PLOTTED!!"  
-
-
-
-def class2(x2, constant):
+# section a has an easy diagonal covariance. The equations simplify to:
+def solve_positive(x2, constant):
     x1 = 1 + np.sqrt(2*(constant-(x2-1)**2))
     return x1
 
-def class1(x2, constant):
+def solve_negative(x2, constant):
     x1 = 1 - np.sqrt(2*(constant-(x2-1)**2))
     return x1
 
-plt.hold('on')
-
 for constant in np.linspace(0.1, 1000, 20):
+    # ellipse1 holds one half of the ellipse, ellipse2 the other 
     ellipse1 = []
     ellipse2 = []
     x_a = []
     for x2 in np.linspace(-100, 100, 5000):
         if (2*(constant-(x2-1)**2)) >= 0:   
-            sol1, sol2 = class1(x2, constant), class2(x2, constant)
+            sol1, sol2 = solve_negative(x2, constant), solve_positive(x2, constant)
             ellipse1.append(sol1)
             ellipse2.append(sol2)
             x_a.append(x2)
-    ellipse1 += (ellipse2)
-    ellipse1.append(ellipse1[0])
-    x_a += x_a[::-1]
-    x_a.append(x_a[0])
-    plt.plot(x_a, ellipse1)
+    ellipse_full = ellipse1 + ellipse2
+    ellipse_full.append(ellipse1[0])     # without this, the ellipse is not plotted whole
+    x_a += x_a[::-1]                    # second half of ellipse - drawn in reverse order
+    x_a.append(x_a[0])                  # to match above
+    plt.plot(x_a, ellipse_full)
 
 plt.ylim((-50, 50))
 plt.xlim((-50, 50))
 
-plt.show()
+plt.title("Part a - Diagonal covariance matrix")
+
+#########################################
+# answer to part b of the question
+###################################
 plt.figure()
 
-for constant in np.linspace(0, 200, 10):
+mu_a = np.mat([[1], [1]])
+sigma_a = np.mat([[2, 0], [0,1]])
+
+
+def part_b(x2, constant):
+    x = Symbol('x')
+    return(solve(2*x**2+8*x+3*x2**2-14*x2-2*x*x2-constant, x))
+
+for constant in np.linspace(10, 100, 5):
     ellipse1 = []
     ellipse2 = []
     x_b = []
-    for x2 in np.linspace(-100, 100, 100):
-        sol1, sol2 = isocontourer(x2, constant)
-        print type(complex(1))
-        if (isinstance(complex(sol1), complex) == False) and (isinstance(complex(sol2), complex) == False): 
-            print sol1, sol2
-            ellipse1.append(sol1)
-            ellipse2.append(sol2)            
+    for x2 in np.linspace(-10, 10, 500):
+        sol1, sol2 = part_b(x2, constant)
+        try:
+            sol1 = float(sol1)
+            sol2 = float(sol2)
+            ellipse2.append(sol1)
+            ellipse1.append(sol2)            
             x_b.append(x2)
-#             print (np.array(ellipse1).flatten()).shape, np.array(x_b).shape
-#                 print isocontourer(x2, constant)
-        else:
-            print sol1, sol2
-            
+#             print sol1, sol2, constant
+        except:
+            pass
+        
     if ellipse1 != []:
-        print ellipse1
-        ellipse1 += (ellipse2)
-        ellipse1.append(ellipse1[0])
+        ellipse1.append(ellipse2[-1])
+        ellipse_full =  (ellipse2)+ellipse1[::-1]
+        ellipse_full.append(ellipse2[0])
         x_b += x_b[::-1]
-        x_b.append(x_b[0])
-        print (np.array(ellipse1).flatten()).shape, np.array(x_b).shape
-        plt.plot(x_b, (ellipse1))
+        x_b.append(x_b[-1])        
+        x_b.append(x_b[-0])        
+        plt.plot(x_b, (ellipse_full))
 
+plt.title("Part b - non-diagonal covariance matrix")
 
+#########################################
+# answer to part c of the question
+###################################
+plt.figure()
 
-
+plt.show()
---
+++ HW3/problem3_1.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+"""
+Illustrate simple contour plotting, contours on an image with
+a colorbar for the contours, and labelled contours.
+
+See also contour_image.py.
+"""
+import matplotlib
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams['xtick.direction'] = 'out'
+matplotlib.rcParams['ytick.direction'] = 'out'
+
+delta = 0.010
+
+#########################################
+# answer to part a of the question
+###################################
+x = np.arange(-4.0, 6.0, delta)
+y = np.arange(-2.0, 4.0, delta)
+X, Y = np.meshgrid(x, y)
+# sigmax, sigmay, mux, muy, sigmaxy
+ZA = mlab.bivariate_normal(X, Y, np.sqrt(2.0), 1.0, 1.0, 1.0, 0.0)
+# Create a simple contour plot with labels using default colors.  The
+# inline argument to clabel will control whether the labels are draw
+# over the line segments of the contour, removing the lines beneath
+# the label
+plt.figure()
+plt.grid()
+CS = plt.contour(X, Y, ZA)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.title('Solution to part a')
+plt.xlim([-5, 6])
+plt.ylim([-5, 6])
+
+#########################################
+# answer to part b of the question
+###################################
+x = np.arange(-8.0, 8.0, delta)
+y = np.arange(-8.0, 8.0, delta)
+X, Y = np.meshgrid(x, y)
+# sigmax, sigmay, mux, muy, sigmaxy
+ZB = mlab.bivariate_normal(X, Y, np.sqrt(3.0), np.sqrt(2.0), -1.0, 2.0, 1.0)
+# Create a simple contour plot with labels using default colors.  The
+# inline argument to clabel will control whether the labels are draw
+# over the line segments of the contour, removing the lines beneath
+# the label
+plt.figure()
+plt.grid()
+CS = plt.contour(X, Y, ZB)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.title('Solution to part b')
+plt.xlim([-8, 6])
+plt.ylim([-2, 6])
+
+#########################################
+# answer to part c of the question
+###################################
+x = np.arange(-2.0, 4.0, delta)
+y = np.arange(-4.0, 6.0, delta)
+X, Y = np.meshgrid(x, y)
+# sigmax, sigmay, mux, muy, sigmaxy
+ZC1 = mlab.bivariate_normal(X, Y, 1.0, np.sqrt(2.0), 0, 2.0, 1.0)
+ZC2 = mlab.bivariate_normal(X, Y, 1.0, np.sqrt(2.0), 2.0, 0.0, 1.0)
+
+ZC = ZC1 - ZC2
+
+# Create a simple contour plot with labels using default colors.  The
+# inline argument to clabel will control whether the labels are draw
+# over the line segments of the contour, removing the lines beneath
+# the label
+plt.figure()
+plt.grid()
+CS = plt.contour(X, Y, ZC)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.title('Solution to part c')
+plt.xlim([-2, 4])
+plt.ylim([-4, 6])
+
+
+#########################################
+# answer to part d of the question
+###################################
+x = np.arange(-2.0, 4.0, delta)
+y = np.arange(-4.0, 6.0, delta)
+X, Y = np.meshgrid(x, y)
+# sigmax, sigmay, mux, muy, sigmaxy
+ZD1 = mlab.bivariate_normal(X, Y, 1.0, np.sqrt(2.0), 0, 2.0, 1.0)
+ZD2 = mlab.bivariate_normal(X, Y, np.sqrt(3), np.sqrt(2.0), 2.0, 0.0, 1.0)
+
+ZD = ZD1 - ZD2
+
+# Create a simple contour plot with labels using default colors.  The
+# inline argument to clabel will control whether the labels are draw
+# over the line segments of the contour, removing the lines beneath
+# the label
+plt.figure()
+plt.grid()
+CS = plt.contour(X, Y, ZD)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.title('Solution to part d')
+plt.xlim([-2, 4])
+plt.ylim([-4, 6])
+
+#########################################
+# answer to part e of the question
+###################################
+x = np.arange(-4.0, 4.0, delta)
+y = np.arange(-6.0, 4.0, delta)
+X, Y = np.meshgrid(x, y)
+# sigmax, sigmay, mux, muy, sigmaxy
+ZE1 = mlab.bivariate_normal(X, Y, 1.0, np.sqrt(2.0), 1.0, 1.0, 0.0)
+ZE2 = mlab.bivariate_normal(X, Y, np.sqrt(2.0), np.sqrt(2.0), -1.0, -1.0, 1.0)
+
+ZE = ZE1 - ZE2
+
+# Create a simple contour plot with labels using default colors.  The
+# inline argument to clabel will control whether the labels are draw
+# over the line segments of the contour, removing the lines beneath
+# the label
+plt.figure()
+plt.grid()
+CS = plt.contour(X, Y, ZE)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.title('Solution to part e')
+plt.xlim([-4, 4])
+plt.ylim([-6, 4])
+
+
+plt.show()

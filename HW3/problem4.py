c54c10e38804b606c8fa51b76622a8b6599ec8c9
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-03-01 15:07:07 -0800

    First rev of problem4 that has some level of part d solved, but horrible error rates yet. This is the before, just you wait for the aftergit add --all

--- HW3/problem4.py
+++ HW3/problem4.py
@@ -46,6 +46,71 @@ def computeCV_Score(clf, data, labels, folds):
 
 ############# IMPORTS ############# 
 
+def train_gauss(data, labels):
+    ############# 
+    # Fit gaussian to each digit
+    ############# 
+    image = np.zeros((28,28))
+    gauss_list = []
+    overall_cov = np.zeros((784, 784))
+    all_cov = []
+    all_prior = []
+    all_mu = []
+    
+    for i in range(0,10):
+        data_label = []
+        index  = 0
+        for label in labels:
+            if(label == i):
+                data_label.append(data[index].flatten())
+            index += 1
+        # data label contains all the data of a certain number
+        # transpose it so that we can take the average across 
+        # all data
+        data_label = np.transpose(np.array(data_label))
+        
+        # mean mu for label 
+        mu = [np.mean(data_label[j]) for j in range(0, 28*28)]
+        
+        # sample covariance
+        cov = np.zeros((784, 784))
+        data_label = data_label.T 
+        for elem in (data_label):
+            cov += np.mat(elem-mu).T*np.mat(elem-mu)
+        cov = cov/len(data_label)
+    
+    
+        # prior probability 
+        prior = len(data_label)/60000.0
+        
+        # visualize this mean
+    #     for j in range(0, 28):
+    #         image[j] = mu[28*j:28*j+28]
+    
+    #     plt.figure()
+    #     plt.imshow(image)
+    #     plt.figure()
+    #     plt.imshow(cov)
+    #     plt.show()
+            
+        print prior, i
+        all_cov.append(cov)
+        all_prior.append(prior)
+        all_mu.append(mu)
+    return all_mu, all_cov, all_prior
+
+def gauss_predict(data, all_mu, overall_cov, all_prior):
+    labelled_list = []
+    for elem in data:
+        prob_list = []
+        for label in range(0, 10):
+            mu = all_mu[label]
+            cov = all_cov[label]
+            n = norm(loc=mu, scale=cov)
+            prob_list.append(np.sum(n.logpdf(elem))*all_prior[label])
+        labelled_list.append(prob_list.index(max(prob_list)))
+    return labelled_list
+
 import scipy as sp
 from scipy import signal
 import numpy as np
@@ -92,81 +157,43 @@ for element in imageData:
 
 imageComplete = zip(imageData, imageLabels)
 
-############# 
-# Fit gaussian to each digit
-############# 
-image = np.zeros((28,28))
-gauss_list = []
-for i in range(0,10):
-    data_label = []
-    index  = 0
-    for label in imageLabels:
-        if(label == i):
-            data_label.append(imageData[index].flatten())
-        index += 1
-    # data label contains all the data of a certain number
-    # transpose it so that we can take the average across 
-    # all data
-    data_label = np.transpose(np.array(data_label))
-    
-    # mean mu for label 
-    mu = [np.mean(data_label[j]) for j in range(0, 28*28)]
-    
-    # sample covariance
-    cov = np.zeros((784, 784))
-    data_label = data_label.T 
-    for elem in (data_label):
-        cov += np.mat(elem-mu).T*np.mat(elem-mu)
-    cov = cov/len(data_label)
-    
-    # prior probability 
-    prior = len(data_label)/60000.0
-    
-    # visualize this mean
-#     for j in range(0, 28):
-#         image[j] = mu[28*j:28*j+28]
-
-#     plt.figure()
-#     plt.imshow(image)
-#     plt.figure()
-#     plt.imshow(cov)
-#     plt.show()
-
-    print prior, i
-    gauss_list.append([i, mu, cov, prior])
-
-gauss_list = np.array(gauss_list)
-print gauss_list.shape
-
-overall_cov = np.mean(cov)
-
-if DEBUG:
-    print 50*'-'
-    print ("Shapes of image data and labels: ", imageData.shape, 
-                                    imageLabels.shape, len(imageComplete))
-    
-    print "Image/Digit 10000:\n", imageComplete[20000]
-    
-############# SET ASIDE VALIDATION DATA (10,000) ############# 
-# VERIFY ORDER
-if DEBUG:    
-    i = 0
-    print 50*'-'
-    for elem in imageComplete:
-        print elem[1]
-        i+=1
-        if i > 5000:
-            break
-        
-# SHUFFLE THE IMAGES
 random.shuffle(imageComplete)
-
-# VERIFY SHUFFLE
-if DEBUG:
-    i = 0    
+shuffledData = []
+shuffledLabels = []
+for elem in imageComplete:
+    shuffledData.append((elem[0]).flatten())                # Use a simple array of pixels as the feature
+    shuffledLabels.append((elem[1][0]))
+    
+errorRate_array = []
+C = np.linspace(1,3,16)                   # array of values for parameter C
+training_Size = [100, 200, 500, 1000, 2000,]# 5000, 10000, 15000]
+for elem in training_Size:
+    all_mu, all_cov, all_prior = train_gauss(shuffledData[:elem], np.array(shuffledLabels[:elem]))
+    overall_cov = np.mean(all_cov)   
+    
+    predicted_Digits = gauss_predict(shuffledData[50000:], all_mu, overall_cov, all_prior)
+    actual_Digits = shuffledLabels[50000:]
+    accuracy = 0.0
+    for elem1, elem2 in zip(predicted_Digits, actual_Digits):
+        if elem1 == elem2:
+            accuracy+=1
+    
+    errorRate_array.append(100-100.0*accuracy/len(predicted_Digits))
+    print "Training Size:", elem 
+    print "Error Rate: ", errorRate_array[-1], "%"
     print 50*'-'
-    for elem in imageComplete:
-        print elem[1]
-        i+=1
-        if i > 5000:
-            break
+
+############# PLOT RESULTS #############     
+
+# Plot error rate vs training size
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('Error Rate Vs Training Size')
+ax.set_ylabel('Training Size')
+ax.set_xlabel('Error Rate')
+ax.plot(training_Size, errorRate_array)
+plt.savefig("./Results/ErrorRate_TrainingSize.png")
+####################################### 
+        
+        
+print 5*"-The End-"

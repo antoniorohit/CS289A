trying so hard to make gradient correct. Tired.

Final Project/feature_extraction/extract_features.py
@@ -20,7 +20,7 @@ def extractFeatures(input_signal):
     :return: features list"""
     
     # compute mfcc list
-    if len(input_signal)==0:
+    if len(input_signal) == 0:
         print("cleaned signal is empty")
         return input_signal
     
@@ -42,10 +42,10 @@ def extractFeatures(input_signal):
     ddelta_list = delta(delta_list, N)
     
     # do not keep first coeff (energy)
-    features_list=list()
+    features_list = list()
     for k in range(len(lpcc)):
-#         features_list += [np.hstack((mfcc_list[k][0:22], lpcc[k][0:22]))]
-        features_list += [mfcc_list[k][1:]]
+        features_list += [np.hstack((mfcc_list[k][0:22], lpcc[k][0:22]))]
+#         features_list += [mfcc_list[k][1:]]
 #         features_list += [lpcc[k][0:15]]
 #         features_list += [np.hstack((mfcc_list[k][0:12], delta_list[k][1:12]))]#, ddelta_list[k][0:]))]
         
@@ -55,7 +55,7 @@ def extractFeatures(input_signal):
     # @TODO WHY DOES THIS HAPPEN?
     for row in features_list:
         for cell in row:
-            if cell!=cell:
+            if cell != cell:
                 print cell
                 return []
     

Final Project/main.py
@@ -17,12 +17,12 @@ prm.params["chunk_size"].set(1)
 
 cleanPickle()
 
-print 20*"#", "Training", 20*"#"
+print 20 * "#", "Training", 20 * "#"
 train()
 
-print 20*"#", "Testing", 20*"#"
+print 20 * "#", "Testing", 20 * "#"
 accuracy = test()
 
-print "Accuracy:", np.around(accuracy,2), "%"
+print "Accuracy:", np.around(accuracy, 2), "%"
 
-print 20*"#", "End of Main. Thank you for Playing", 20*"#"
+print 20 * "#", "End of Main. Thank you for Playing", 20 * "#"

Final Project/train.py
@@ -37,7 +37,7 @@ def train():
     
     scoreBuffer = []
     
-    C = np.linspace(.01, 1000. , 4)                   # array of values for parameter C
+    C = np.linspace(.01, 1000. , 4)  # array of values for parameter C
     depths = [5, 10, 25]
     
     ############# FORESTS/TREES ############# 
@@ -63,13 +63,16 @@ def train():
     # HOW DOES THIS DECISION TREE DO ON THE SAME DATA IT FIT ON? (should be 100%)
     accuracy = 0
     predicted_Labels = clf.predict(data)
+    index = 0
     for (elem1, elem2) in zip(predicted_Labels, labels):
         if elem1 == elem2:
-            accuracy+=1
+            accuracy += 1
         else:
-            pass
+            print 'ERROR'
+            write_wave((1, 2, 44100), data[index], './Errors/cleaned_audio_' + str(index) + '.wav')
+        index += 1
     print "Predicted:", len(predicted_Labels), sum(predicted_Labels)
-    print "Accuracy of CLF on training data itself:", np.around(100.0*accuracy/len(predicted_Labels),2), "%"
+    print "Accuracy of CLF on training data itself:", np.around(100.0 * accuracy / len(predicted_Labels), 2), "%"
      
      
     ############# SVM ############# 
@@ -129,5 +132,5 @@ def train():
 #     clf = KNeighborsClassifier(n_neighbors=num_neighbors[maxScore_Index])
 
     clf.fit(data, labels)
-    pickle.dump(clf, open(pickle_directory+"clf.p", "wb"))        
+    pickle.dump(clf, open(pickle_directory + "clf.p", "wb"))        
     print "Done saving the CLF!"

Final Project/utils.py
@@ -6,6 +6,9 @@ detector
 
 @author: antonio
 '''
+import sys
+sys.path.append("./VAD")
+
 import wave
 import numpy as np
 import parameters as prm
@@ -13,6 +16,7 @@ import os
 from extract_features import extractFeatures
 import cPickle as pickle
 import random
+from simple import remove_silence
 
 def get_RawSignal(file_name):
     '''Opens filename, and returns (params, rawSignal)'''
@@ -27,12 +31,12 @@ def get_RawSignal(file_name):
 
 def frame_chunks(rawSignal):
     '''Breaks rawSignal into chunk_size blocks, RETURNS the framedSignal'''
-    chunk_size = prm.params["chunk_size"].get()*prm.params["sample_rate"].get()
+    chunk_size = prm.params["chunk_size"].get() * prm.params["sample_rate"].get()
     framedRawSignal = []
     index = 0
     while index < (len(rawSignal) - chunk_size):
-        framedRawSignal.append(rawSignal[index:index+chunk_size])
-        index += chunk_size             #non-overlapping
+        framedRawSignal.append(rawSignal[index:index + chunk_size])
+        index += chunk_size  # non-overlapping
     
     # Zero padding for last sample (unnecessary?)    (remove)
 #     if index != len(rawSignal):
@@ -41,9 +45,9 @@ def frame_chunks(rawSignal):
 
     return framedRawSignal
 
-def write_wave(params, cleaned_signal, output_fname = 'cleaned_audio.wav', output_dir = ''):
+def write_wave(params, cleaned_signal, output_fname='cleaned_audio.wav', output_dir=''):
     '''Creates a wave file from an input signal array'''
-    wf = wave.open(output_dir+output_fname, 'wb')
+    wf = wave.open(output_dir + output_fname, 'wb')
     wf.setnchannels(params[0])
     wf.setsampwidth(params[1])
     wf.setframerate(params[2])
@@ -54,8 +58,8 @@ def write_wave(params, cleaned_signal, output_fname = 'cleaned_audio.wav', outpu
 
 
 def extract_Gender_Label(directory):
-    folder = directory+"/etc/"
-    readme =  open(folder+"README").read().strip().lower()
+    folder = directory + "/etc/"
+    readme = open(folder + "README").read().strip().lower()
     if("female" in readme):
         label = 0
     elif("male" in readme):
@@ -72,13 +76,13 @@ def extract_Data(data_directory):
     for folder in os.listdir(data_directory):
         if "." not in folder and "an4" not in folder and "anonymous" not in folder:
             print folder
-            directory = data_directory+folder
+            directory = data_directory + folder
             folder = "/wav/"
             try:
-                for file_name in os.listdir(directory+folder):
+                for file_name in os.listdir(directory + folder):
                     if file_name[-3:] == "wav":
                         label = extract_Gender_Label(directory)
-                        _, rawSignal = get_RawSignal(directory+folder+file_name)
+                        _, rawSignal = get_RawSignal(directory + folder + file_name)
                         framedRawSignal = (frame_chunks(rawSignal))
                         for chunk in framedRawSignal:
                             feature_list = extractFeatures(chunk)
@@ -96,7 +100,7 @@ def select_Data_Subset(data, labels, fraction):
     # It works - I checked
     trainingComplete = np.array(zip(data, labels))
     data_len = len(data)
-    indices = np.random.randint(data_len, size=int(data_len*fraction))
+    indices = np.random.randint(data_len, size=int(data_len * fraction))
     trainingComplete_replaced = trainingComplete[indices, :]
     trainingData = trainingComplete_replaced[:, 0].tolist()
     trainingLabels = trainingComplete_replaced[:, 1].tolist()
@@ -107,7 +111,7 @@ def select_Data_Subset(data, labels, fraction):
 def getTrainData_Pickle(method="voxforge"):
     if method == "voxforge":
         data, labels = getData_Voxforge()
-    else:   # test_protocol
+    else:  # test_protocol
         data, labels = getData_TestProtocol("train")
     
     return data, labels
@@ -118,10 +122,10 @@ def getData_Voxforge():
 
     try:
         print "Loading the Data... (may take a while)"
-        data = pickle.load(open(pickle_directory+"data_vf.p", "rb"))
-        labels = pickle.load(open(pickle_directory+"labels_vf.p", "rb"))
+        data = pickle.load(open(pickle_directory + "data_vf.p", "rb"))
+        labels = pickle.load(open(pickle_directory + "labels_vf.p", "rb"))
         print "Data Loaded, Good to Go!"
-    except Exception as excp:               # data_vf.p doesnt exist
+    except Exception as excp:  # data_vf.p doesnt exist
         print "Exception:", excp
         data, labels = extract_Data(voxforge_directory)
         print "Flattening ze Data"
@@ -135,7 +139,7 @@ def getData_Voxforge():
         data_female = []
         
         for elem in zip(data, labels):
-            if elem[1] == 1: # male
+            if elem[1] == 1:  # male
                 data_male.append(elem[0])
             else:
                 data_female.append(elem[0])
@@ -164,8 +168,8 @@ def getData_Voxforge():
             labels.append(elem[1])
         
         print "Saving the Data... (may take a while)"
-        pickle.dump(data, open(pickle_directory+"data_vf.p", "wb"))        
-        pickle.dump(labels, open(pickle_directory+"labels_vf.p", "wb"))        
+        pickle.dump(data, open(pickle_directory + "data_vf.p", "wb"))        
+        pickle.dump(labels, open(pickle_directory + "labels_vf.p", "wb"))        
         print "Data Saved, Good to Go!"
     
         print "Shapes of Data (male, female) and Labels", np.shape(data_male), np.shape(data_female), np.shape(labels)
@@ -182,31 +186,37 @@ def getData_TestProtocol(source="train"):
     
     try:
         print "Loading the Data... (may take a while)"
-        data = pickle.load(open(pickle_directory+"data_tp_"+ str(source) + ".p", "rb"))
-        labels = pickle.load(open(pickle_directory+"labels_tp_"+ str(source) + ".p", "rb"))
+        data = pickle.load(open(pickle_directory + "data_tp_" + str(source) + ".p", "rb"))
+        labels = pickle.load(open(pickle_directory + "labels_tp_" + str(source) + ".p", "rb"))
         print "Data Loaded, Good to Go!"
-    except Exception as excp:               # data_vf.p doesnt exist
+    except Exception as excp:  # data_vf.p doesnt exist
         print "Exception:", excp
         for filename in os.listdir(data_dir):
             filename = filename.lower()
-            if source in filename and filename[-3:] == "wav" and "," not in filename and "audio" not in filename and "mac" not in filename:
+            if source in filename and filename[-3:] == "wav" and "," not in filename and "audio" not in filename and "android" not in filename:
 #                 if source == "train":
 #                     if "clean" not in filename:
 #                         continue
-                if source == "test":
-                    if "clean" not in filename:
-                        continue
+#                 if source == "test":
+#                     if "clean" not in filename:
+#                         continue
                 print filename
         
                 #######################################
                 # GET RAW SIGNAL 
                 #######################################
-                params, rawSignal = get_RawSignal(data_dir+filename)
+                params, rawSignal = get_RawSignal(data_dir + filename)
                             
+                #######################################
+                # CLEAN SAMPLES 
+                #######################################
+                fs, cleanSignal = remove_silence(params[2], rawSignal)
+                
                 #######################################
                 # FRAME SAMPLES 
                 #######################################
-                framedRawSignal = frame_chunks(rawSignal)
+                framedRawSignal = frame_chunks(cleanSignal)
+
                 
                 #######################################
                 # ASCERTAIN GENDER 
@@ -250,8 +260,8 @@ def getData_TestProtocol(source="train"):
 
         
         print "Saving the Data... (may take a while)"
-        pickle.dump(data, open(pickle_directory+"data_tp_"+ str(source) + ".p", "wb"))        
-        pickle.dump(labels, open(pickle_directory+"labels_tp_"+ str(source) + ".p", "wb"))        
+        pickle.dump(data, open(pickle_directory + "data_tp_" + str(source) + ".p", "wb"))        
+        pickle.dump(labels, open(pickle_directory + "labels_tp_" + str(source) + ".p", "wb"))        
         print "Data Saved, Good to Go!"
 
     print "Shapes of Data and Labels", np.shape(data), np.shape(labels)
@@ -282,17 +292,17 @@ def computeCV_Score(clf, data, labels, folds):
             clf_local.fit(data[i], labels[i])
             # For each validation performed (k-1 total) on a fold
             for j in range(folds):
-                if(j!=i):
+                if(j != i):
                     predicted_Labels = clf_local.predict(data[j])
                     for (elem1, elem2) in zip(predicted_Labels, labels[j]):
                         if elem1 == elem2:
-                            accuracy+=1
+                            accuracy += 1
                         else:
                             pass
-                j+=1
+                j += 1
             print "Predicted:", len(predicted_Labels), sum(predicted_Labels)
-            scores.append(100.0*accuracy/((folds-1)*len(predicted_Labels)))
-            i+=1
+            scores.append(100.0 * accuracy / ((folds - 1) * len(predicted_Labels)))
+            i += 1
         except Exception, e:
             pass
 #             print str(e)
@@ -302,4 +312,4 @@ def cleanPickle():
     pickle_dir = prm.params["pickle_directory"].get()
 
     for filename in os.listdir(pickle_dir):
-        os.remove(pickle_dir+filename)
+        os.remove(pickle_dir + filename)

HW6/main.py
@@ -15,25 +15,25 @@ import time
 testFileMNIST = "./digit-dataset/test.mat"
 trainFileMNIST = "./digit-dataset/train.mat"
     
-trainMatrix = io.loadmat(trainFileMNIST)                 # Dictionary
-testMatrix = io.loadmat(testFileMNIST)                   # Dictionary
+trainMatrix = io.loadmat(trainFileMNIST)  # Dictionary
+testMatrix = io.loadmat(testFileMNIST)  # Dictionary
 
 ############# GET DATA ############# 
-print 20*"#", "Getting Data", 20*"#"
+print 20 * "#", "Getting Data", 20 * "#"
 testData = np.array(testMatrix['test_images'])
-testData = np.rollaxis(testData, 2, 0)                # move the index axis to be the first 
+testData = np.rollaxis(testData, 2, 0)  # move the index axis to be the first 
 
 imageData = np.array(trainMatrix['train_images'])
-imageData = np.rollaxis(imageData, 2, 0)                # move the index axis to be the first 
+imageData = np.rollaxis(imageData, 2, 0)  # move the index axis to be the first 
 imageLabels = np.array(trainMatrix['train_labels'])
 
 ############# PROCESS DATA ############# 
-features = "raw"            # or "raw"
+features = "raw"  # or "raw"
 
 if features == "raw":
     # Non malik - raw, shuffled, and labels converted to one-of-10-high format
     imageData, imageLabels, _ = getDataNonMalik(zip(imageData, imageLabels))
-    shufTestData, _, _ = getDataNonMalik(zip(testData, np.ones((len(testData),1))))
+    shufTestData, _, _ = getDataNonMalik(zip(testData, np.ones((len(testData), 1))))
 else:
     # Malik - HOG, shuffled, and labels converted to one-of-10-high format
     imageData, imageLabels = getDataPickle(imageData, imageLabels, "train")
@@ -45,13 +45,13 @@ print "Image Data Shape", dataShape
 print "Test Data Shape", np.shape(shufTestData)
 
 ############# DATA PARTIONING ############# 
-print 20*"#", "Cross Validation", 20*"#"
-crossValidation_Data= []
+print 20 * "#", "Cross Validation", 20 * "#"
+crossValidation_Data = []
 crossValidation_Labels = []
 k = 10 
-lengthData = 10000          # do CV with a subset of the entire dataset so it takes less time (eg 5-10k samples)
+lengthData = 10000  # do CV with a subset of the entire dataset so it takes less time (eg 5-10k samples)
 stepLength = k
-for index in range(0,k):
+for index in range(0, k):
     crossValidation_Data.append(imageData[index:lengthData:stepLength])
     crossValidation_Labels.append(imageLabels[index:lengthData:stepLength])
 if lengthData < 50000:
@@ -69,49 +69,49 @@ print "Gamma:", clf.gamma
 
 score, clf = computeCV_Score(clf, crossValidation_Data, crossValidation_Labels, k)
 
-print "NN Cross-Val Score:", np.around(np.mean(score),1), "%"
+print "NN Cross-Val Score:", np.around(np.mean(score), 1), "%"
 
 ############# VALIDATION SET ############# 
-print 20*"#", "Validation Set Accuracy", 20*"#"
+print 20 * "#", "Validation Set Accuracy", 20 * "#"
 pred_labels = clf.predict(validationSet_Data)
 accuracy = 0
 for (elem1, elem2) in zip(pred_labels, validationSet_Labels):
     elem2 = elem2.tolist().index(1)
     if elem1 == elem2:
-        accuracy+=1
+        accuracy += 1
     else:
         pass
 
-print "Validation Set Accuracy:", 100.0*accuracy/len(pred_labels), "%"
+print "Validation Set Accuracy:", 100.0 * accuracy / len(pred_labels), "%"
 
 ############# MEGA TRAINING ############# 
-print 20*"#", "Training on all Data...", 20*"#"
+print 20 * "#", "Training on all Data...", 20 * "#"
 start = time.time()
 clf.fit(imageData, imageLabels)
-print "Training Time for entire NN:", np.around((time.time()-start)/60., 1), "minutes"      # 13 minutes
+print "Training Time for entire NN:", np.around((time.time() - start) / 60., 1), "minutes"  # 13 minutes
 
-try:    # @TODO can the CLF really be saved?
+try:  # @TODO can the CLF really be saved?
     print "Saving the NN CLF..."
-    pickle.dump(clf, open("./Results/" +"nn_clf_" + features + ".p", "wb"))        
+    pickle.dump(clf, open("./Results/" + "nn_clf_" + features + ".p", "wb"))        
     print "Done saving the NN CLF!"
 except Exception, e:
     print str(e)
     
 ############# VALIDATION SET (AGAIN) ############# 
-print 20*"#", "Validation Set Accuracy", 20*"#"
+print 20 * "#", "Validation Set Accuracy", 20 * "#"
 pred_labels = clf.predict(validationSet_Data)
 accuracy = 0
 for (elem1, elem2) in zip(pred_labels, validationSet_Labels):
     elem2 = elem2.tolist().index(1)
     if elem1 == elem2:
-        accuracy+=1
+        accuracy += 1
     else:
         pass
 
-print "Validation Set Accuracy:", 100.0*accuracy/len(pred_labels), "%"
+print "Validation Set Accuracy:", 100.0 * accuracy / len(pred_labels), "%"
 
 ############# FOR KAGGLE ############# 
-print 20*"#", "Predicting for Kaggle", 20*"#"
+print 20 * "#", "Predicting for Kaggle", 20 * "#"
 indices = np.array(range(1, len(shufTestData) + 1))
 print np.shape(shufTestData)
 pred_labels = clf.predict(shufTestData)
@@ -119,4 +119,4 @@ pred_labels = clf.predict(shufTestData)
 kaggle_format = np.vstack(((indices), pred_labels)).T
 np.savetxt("./Results/digits.csv", kaggle_format, delimiter=",", fmt='%d,%d', header='Id,Category', comments='') 
 
-print 20*"#", "The End !", 20*"#"
+print 20 * "#", "The End !", 20 * "#"

HW6/neuralNet.py
@@ -17,47 +17,53 @@ class Digit_NN(object):
         self.nin = dataShape
         self.nout = 10
         self.nhidden = 200
-        self.gamma = 10**-3
+        self.gamma = 10 ** -3
         self.yHat = []
         # initialize weights
-        self.W1 = 0.001*np.random.randn(dataShape + 1, self.nhidden)    
-        self.W2 = 0.001*np.random.randn(self.nhidden+1, self.nout)
+        self.W1 = 0.001 * np.random.randn(dataShape + 1, self.nhidden)    
+        self.W2 = 0.001 * np.random.randn(self.nhidden + 1, self.nout)
         if cost == "MSE":
             self.costFunction = self.costFunction_mse
-        else:   # entropy
+        else:  # entropy
+            print "WARNING _ ENTROPY NOT IMPLEMENTED "
             self.costFunction = self.costFunction_entropy
 
     def derivative_tanh(self, z):
         """ Computes the derivative of the sigmoid of z"""
         z = np.array(z)
-        return (1-np.square(np.tanh(z)))
+        return (1 - np.square(np.tanh(z)))
 
     def derivative_sig(self, z):
         """ Computes the derivative of the sigmoid of z"""
         z = np.array(z)
-        return np.exp(-z)/(np.square(1+np.exp(-z)))
+        return np.exp(-z) / (np.square(1 + np.exp(-z)))
     
     def sigmoid(self, z):
         """ Computes the sigmoid of z"""
-        return (1/(1+np.exp(-z)))
+        return (1 / (1 + np.exp(-z)))
 
     
     def forward(self, X):
+        X = np.hstack((X, np.ones((len(X), 1))))
         self.z2 = np.dot(X, self.W1)
-        self.a2 = np.hstack((np.tanh(self.z2), np.ones((np.shape(X)[0],1))))
+        self.a2 = np.hstack((np.tanh(self.z2), np.ones((np.shape(X)[0], 1))))
         self.z3 = np.dot(self.a2, self.W2)
         self.yHat = self.sigmoid(self.z3)
     
     def backprop(self, x, y):
+        x = np.hstack((x, np.ones((len(x), 1))))
         # W2
-        delta3 = np.multiply(-(y-self.yHat), self.derivative_sig(self.z3))
+        # a2: 1x(nhidden+1), delta3: 1xnout
+        delta3 = np.multiply(-(y - self.yHat), self.derivative_sig(self.z3))
         dJdW2 = np.dot(self.a2.T, delta3)
-        self.W2 -= dJdW2*self.gamma
+        self.W2 -= dJdW2 * self.gamma
         
         # W1
-        delta2 = np.multiply(np.dot(delta3, self.W2.T)[:,self.nhidden-1], self.derivative_tanh(self.z2))
+        # delta2: 1x200 
+        # @TODO - drop the first or last coeff of W2??? i.e. 1: or :-1
+        delta2 = np.multiply(np.dot(delta3, self.W2.T)[:,:-1], self.derivative_tanh(self.z2))
         dJdW1 = np.dot(x.T, delta2)
-        self.W1 -= dJdW1*self.gamma
+        self.W1 -= dJdW1 * self.gamma
         
         return dJdW1, dJdW2
 
@@ -65,65 +71,60 @@ class Digit_NN(object):
     def fit(self, data, labels):
         labels = np.array(labels)
         data = np.array(data)
-        data = np.hstack((data, np.ones((len(data),1))))
         data_len = len(data)
         completeData = zip(data, labels)
-        epsilon = 10**0
-        cost = deque((self.costFunction(data,labels))*np.ones(10))
+        epsilon = 2*10 ** -4
+        cost = deque((self.costFunction(data, labels)) * np.ones(10))
         curr_cost = np.mean(cost)
         delta = 1
         i = 0
-        j=0
+        j = 0
         startTime = 0
         while 1:
-            x,y = completeData[np.random.randint(data_len, size=1)[0]]
+            x, y = completeData[np.random.randint(data_len, size=1)[0]]
+
+            numgrad = self.computeNumericalGradient(self, x, y)
             self.forward(np.matrix(x))
             dJdW1, dJdW2 = self.backprop(np.matrix(x), y)
-            numgrad = self.computeNumericalGradient(self, x, y)
-            print "DJ shapes:", np.shape(dJdW1.ravel().T), np.shape(dJdW2.ravel().T)
-            grad = np.concatenate((np.array(dJdW1.ravel().T.tolist()), np.array(dJdW2.ravel().T.tolist())))
-            
-            print "Shapes", np.shape(grad), np.shape(numgrad)
-            
-            numer = np.linalg.norm(grad-numgrad)
-            denom = np.linalg.norm(grad+numgrad)
+            grad = np.concatenate((dJdW1.ravel().T.tolist(), dJdW2.ravel().T.tolist())).ravel()
+            numer = np.linalg.norm(grad - numgrad)
+            denom = np.linalg.norm(grad + numgrad)
             print numer
             print denom
-            print "The factor!!", numer/denom
+            print "The factor!!", numer / denom
 
-            if i %10000 == 0:
+            if i % 10000 == 0:
 #                 print "Loop Time:", time.time()-startTime
-                curr_cost = (self.costFunction(data,labels))    
+                curr_cost = (self.costFunction(data, labels))    
                 old_cost = np.mean(cost)
                 cost.append(curr_cost)
                 cost.popleft()
                 delta = np.mean(cost) - old_cost
                 if(np.abs(delta) <= epsilon) or i > 1200000 or curr_cost <= epsilon:
-                    j+=1
-                    if(j>5 or curr_cost == 0):
+                    j += 1
+                    if(j > 5 or curr_cost == 0):
                         print "Cost and Delta:", cost, delta
                         self.gamma = 0.001
                         break   
                 else:
-                    j=0
+                    j = 0
 
-                print "i, Cost, Delta:", i, np.around(curr_cost), delta
-                if i > 10000:
-                    self.gamma = .1/np.sqrt(i)
+                print "i, Cost, Delta:", i, np.around(curr_cost, 3), np.around(delta, 6)
+                if i > 200000:
+                    self.gamma = 0.1 / np.sqrt(i)
                         
 #                 startTime = time.time()
 
-            i+=1
+            i += 1
         return self.W1, self.W2
     
     def predict(self, testData):
         # append bias of 1
-        testData = np.hstack((testData, np.ones((len(testData),1))))
         predicted = []
         self.forward(np.matrix(testData))
         for elem in testData:
             self.forward(np.matrix(elem))
-            nn_label = np.around(self.yHat,0).T.tolist()
+            nn_label = np.around(self.yHat, 0).T.tolist()
             max_value = np.max(nn_label)
             max_index = nn_label.index(max_value)
 #             print max_index
@@ -133,27 +134,27 @@ class Digit_NN(object):
     
     
     def costFunction_mse(self, x, y):
-        #Compute cost for given X,y, use weights already stored in class.
-        self.forward(np.matrix(x))      # this forward pass is over the entire training set. 
-        J = 0.5*np.mean(np.sum(np.square(y-self.yHat)))
+        # Compute cost for given X,y, use weights already stored in class.
+        self.forward(np.matrix(x))  # this forward pass is over the entire training set. 
+        J = 0.5 * np.sum(np.mean(np.square(y - self.yHat), 0).T)  # mean over all training examples, sum over outputs         
         return J 
     
     def costFunction_entropy(self, x, y):
-        #Compute cost for given X,y, use weights already stored in class.
+        # Compute cost for given X,y, use weights already stored in class.
         self.forward(np.matrix(x))
-        stability_term = 10**-8
-        term1 = np.multiply(y, np.log(self.yHat+10**-8))
-        term2 = np.multiply((1-y), np.log(1-self.yHat + 10**-8))
-        error_matrix = term1+term2
+        stability_term = 10 ** -8
+        term1 = np.multiply(y, np.log(self.yHat + stability_term))
+        term2 = np.multiply((1 - y), np.log(1 - self.yHat + stability_term))
+        error_matrix = term1 + term2
         J = -np.sum(np.sum(error_matrix))
         return J
 
     def setParams(self, params):
-        #Set W1 and W2 using single paramater vector.
+        # Set W1 and W2 using single paramater vector.
         W1_start = 0
         W1_end = self.nhidden * (self.nin + 1)
         self.W1 = np.reshape(params[W1_start:W1_end], (self.nin + 1 , self.nhidden))
-        W2_end = W1_end + (self.nhidden + 1)*self.nout
+        W2_end = W1_end + (self.nhidden + 1) * self.nout
         self.W2 = np.reshape(params[W1_end:W2_end], (self.nhidden + 1, self.nout))
 
 
@@ -161,24 +162,24 @@ class Digit_NN(object):
         paramsInitial = np.concatenate((N.W1.ravel(), N.W2.ravel()))
         numgrad = np.zeros(paramsInitial.shape)
         perturb = np.zeros(paramsInitial.shape)
-        e = 1e-6
+        epsilon = 10 ** -5
 
         for p in range(len(paramsInitial)):
-            #Set perturbation vector
-            perturb[p] = e
+            # Set perturbation vector
+            perturb[p] = epsilon
             N.setParams(paramsInitial + perturb)
             loss2 = N.costFunction(X, y)
             
             N.setParams(paramsInitial - perturb)
             loss1 = N.costFunction(X, y)
 
-            #Compute Numerical Gradient
-            numgrad[p] = (loss2 - loss1) / (2*e)
+            # Compute Numerical Gradient
+            numgrad[p] = (loss2 - loss1) / (2.0 * epsilon)
 
-            #Return the value we changed to zero:
+            # Return the value we changed to zero:
             perturb[p] = 0
             
-        #Return Params to original value:
+        # Return Params to original value:
         N.setParams(paramsInitial)
 
         return numgrad 
@@ -187,7 +188,7 @@ class Digit_NN(object):
         new_labels = []
         denominator_sm = sum(np.exp(labels))
         for i in range(10):
-            new_labels.append(np.exp(labels[i])/denominator_sm)
+            new_labels.append(np.exp(labels[i]) / denominator_sm)
         max_value = max(new_labels)
         max_index = new_labels.index(max_value)
         return max_index

HW6/utils.py
@@ -20,8 +20,8 @@ def blockshaped(arr, nrows, ncols):
     each subblock preserving the "physical" layout of arr.
     """
     h, w = arr.shape
-    return (arr.reshape(h//nrows, nrows, -1, ncols)
-               .swapaxes(1,2)
+    return (arr.reshape(h // nrows, nrows, -1, ncols)
+               .swapaxes(1, 2)
                .reshape(-1, nrows, ncols))
 
 def computeCV_Score(clf, data, labels, folds):
@@ -42,18 +42,18 @@ def computeCV_Score(clf, data, labels, folds):
         clf_local.fit(data[i], labels[i])
         # For each validation performed (k-1 total) on a fold
         for j in range(folds):
-            if(j!=i):
+            if(j != i):
                 predicted_Digits = clf_local.predict(data[j])
                 for (elem1, elem2) in zip(predicted_Digits, labels[j]):
                     elem2 = elem2.tolist().index(1)
                     if elem1 == elem2:
-                        accuracy+=1
+                        accuracy += 1
                     else:
                         pass
-            j+=1
-        scores.append(100.0*accuracy/((folds-1)*len(predicted_Digits)))
-        print "Accuracy:", np.around(scores[-1],1), "%"
-        i+=1
+            j += 1
+        scores.append(100.0 * accuracy / ((folds - 1) * len(predicted_Digits)))
+        print "Accuracy:", np.around(scores[-1], 1), "%"
+        i += 1
     return np.array(scores), clf_local
 
 def getDataNonMalik(imageComplete):
@@ -65,7 +65,7 @@ def getDataNonMalik(imageComplete):
     shuffledData = []
     shuffledLabels = []
     for elem in imageComplete:
-        shuffledData.append((elem[0]).flatten())                # Use a simple array of pixels as the feature
+        shuffledData.append((elem[0]).flatten())  # Use a simple array of pixels as the feature
         shuffledLabels.append((elem[1][0]))
     
     imageLabels_Vector = np.zeros((len(shuffledLabels), 10))
@@ -96,7 +96,7 @@ def getDataMalik(gauss_bool, imageData, imageLabels):
     # Ink Normalization
     ############# 
     for i in range(len(imageData)):
-        imageData[i]-=np.mean(imageData[i])
+        imageData[i] -= np.mean(imageData[i])
         aux_norm = np.linalg.norm(imageData[i])
         if aux_norm != 0:
             imageData[i] /= aux_norm
@@ -106,21 +106,21 @@ def getDataMalik(gauss_bool, imageData, imageLabels):
     shuffledData = []
     shuffledLabels = []
 
-    n_bins=9
+    n_bins = 9
     for ind in range(len(imageComplete)):
         if ind % 300 == 0:
-            print 'feature extraction :' + str(np.around(ind*100./len(imageComplete), 1))+ ' % over'
+            print 'feature extraction :' + str(np.around(ind * 100. / len(imageComplete), 1)) + ' % over'
         
         if gauss_bool:
-            gaussFirst_x = filters.gaussian_filter1d(imageComplete[ind][0], 1, order = 1, axis = 0)
-            gaussFirst_y = filters.gaussian_filter1d(imageComplete[ind][0], 1, order = 1, axis = 1)
+            gaussFirst_x = filters.gaussian_filter1d(imageComplete[ind][0], 1, order=1, axis=0)
+            gaussFirst_y = filters.gaussian_filter1d(imageComplete[ind][0], 1, order=1, axis=1)
             ori = np.array(np.arctan2(gaussFirst_y, gaussFirst_x))
 
         else:
             grad_filter = np.array([[-1, 0, 1]])
             gradx = signal.convolve2d(imageComplete[ind][0], grad_filter, 'same')
             grady = signal.convolve2d(imageComplete[ind][0], np.transpose(grad_filter), 'same')
-            mag = np.sqrt(gradx**2+grady**2)
+            mag = np.sqrt(gradx ** 2 + grady ** 2)
             ori = np.array(np.arctan2(grady, gradx))
         
         ori_4_hist = list()
@@ -140,7 +140,7 @@ def getDataMalik(gauss_bool, imageData, imageLabels):
                 
         features = np.append(ori_4_hist, ori_7_hist)
         features = np.append(features, mag)
-        features = (features - np.mean(features))/np.linalg.norm(features)
+        features = (features - np.mean(features)) / np.linalg.norm(features)
         
         shuffledData.append(features)
         shuffledLabels.append((imageComplete[ind][1]))
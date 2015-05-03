ff322dd910d77e337f8a46d873ac3f982fe427e7
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-05-03 16:24:06 -0700

    Completed warm up

--- HW7/Jokes/main.py
+++ HW7/Jokes/main.py
@@ -9,11 +9,39 @@ from scipy import io
 
 ############# FILE STUFF ############# 
 trainFile = "./joke_data/joke_train.mat"
-    
+validationFile = "./joke_data/validation.txt"
+testFile = "./joke_data/query.txt"
+
 trainMatrix = io.loadmat(trainFile)  # Dictionary
+vf = open(validationFile, "rb")
+tf = open(testFile, "rb")
 
 ############# GET DATA ############# 
 print 20 * "#", "Getting Data", 20 * "#"
 jokeData = np.array(trainMatrix['train'])
 
 print np.shape(jokeData)
+
+validationData = []
+for line in vf.readlines():
+    validationData.append(map(int, line.strip().split(",")))
+    
+testData = []
+for line in tf.readlines():
+    testData.append(map(int, line.strip().split(",")))
+
+print np.shape(testData), np.shape(validationData)
+
+# AVerage score
+av_score = []
+for i in range(np.shape(jokeData)[1]):
+    av_score.append(np.mean([x[i] for x in jokeData if not np.isnan(x[i])]))
+    
+print av_score
+
+accuracy = 0
+for elem in validationData:
+    if elem[2]==1 and av_score[elem[1]-1] > 0:
+        accuracy+=1
+
+print "Simple Accuracy:", np.around(100.0*accuracy/len(validationData)), "%"

4726005098fc8ab20fbbac67cc3d5f9033a985c9
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-05-04 19:15:05 -0700

    Finished 2.2 HW7

---
+++ HW7/Jokes/knn.py
@@ -0,0 +1,26 @@
+'''
+Created on May 4, 2015
+
+@author: antonio
+'''
+import numpy as np
+
+class KNN(object):
+    def __init__(self, k):
+        self.k = k
+    
+    def fit(self, X):
+        X = np.array(X)
+        dataLen = len(X)
+        self.neighbours = np.zeros((dataLen, self.k))
+        for i in range(100):
+            distanceBuf = []
+            for j in range(dataLen):
+                if i!=j:
+                    distanceBuf.append(np.sum(np.square(X[i]-X[j])))
+            sortedDistanceBuf = sorted(distanceBuf)
+            self.neighbours[i]=[distanceBuf.index(x) for x in sortedDistanceBuf[:self.k]]
+            
+            
+            
+        
--- HW7/Jokes/main.py
+++ HW7/Jokes/main.py
@@ -6,6 +6,7 @@ Created on May 3, 2015
 ############# IMPORTS ############# 
 import numpy as np
 from scipy import io
+from knn import KNN
 
 ############# FILE STUFF ############# 
 trainFile = "./joke_data/joke_train.mat"
@@ -20,7 +21,7 @@ tf = open(testFile, "rb")
 print 20 * "#", "Getting Data", 20 * "#"
 jokeData = np.array(trainMatrix['train'])
 
-print np.shape(jokeData)
+print "Joke Data Shape:", np.shape(jokeData)
 
 validationData = []
 for line in vf.readlines():
@@ -30,19 +31,40 @@ testData = []
 for line in tf.readlines():
     testData.append(map(int, line.strip().split(",")))
 
-print np.shape(testData), np.shape(validationData)
+print "Validation, Test Data Shape:", np.shape(validationData), np.shape(testData)
 
 ############# SIMPLE SYS ############# 
 # Average score
 av_score = []
+accuracy = 0
 for i in range(np.shape(jokeData)[1]):
     av_score.append(np.mean([x[i] for x in jokeData if not np.isnan(x[i])]))
     
-accuracy = 0
 for elem in validationData:
-    if elem[2]*av_score[elem[1]-1] > 0 or (elem[2]==0 and av_score[elem[1]-1] < 0):
+    if elem[2]*av_score[elem[1]-1] > 0 or (elem[2]==0 and av_score[elem[1]-1] <= 0):
         accuracy+=1
 
 print "Simple Accuracy:", np.around(100.0*accuracy/len(validationData)), "%"
 
-############# PERSONAL PREF ############# 
+############# PERSONAL PREF #############
+# replace nan by 0
+for i in range(len(jokeData)):
+    jokeData[i] = [0 if np.isnan(x) else x for x in jokeData[i] ]
+    
+for k in [10, 100, 1000, 10000]:
+    print "K Value:", k
+    knn = KNN(k)
+    knn.fit(jokeData)
+    neighbours = knn.neighbours
+    av_score = []
+    accuracy = 0
+    for i in range(100):
+        av_score.append(np.mean([jokeData[ind] for ind in neighbours[i]]))
+        
+    for elem in validationData:
+        if elem[2]*av_score[elem[1]-1] > 0 or (elem[2]==0 and av_score[elem[1]-1] <= 0):
+            accuracy+=1
+    
+    print "Pref Accuracy:", np.around(100.0*accuracy/len(validationData)), "%"
+        
+     
